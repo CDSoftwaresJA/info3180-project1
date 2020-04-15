c15901c52a62d7be56c4dbf8f321b261ab28e3e8
Ubuntu <ubuntu@ip-172-31-22-199.us-west-2.compute.internal>
2020-04-15 16:48:22 +0000

    Second commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='Project 1',
+   version='1.0',
+   description='Project 1',
+   author='Carl Duncan',
+   author_email='carlduncan64@gmail.com',
+   packages=['Project1'],  #same as name
+)
