69323f1a109fa59e43c0a2b0a17ba009f8b50d01
Ubuntu <ubuntu@ip-172-31-22-199.us-west-2.compute.internal>
2020-04-15 19:17:26 +0000

    commit

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
-from flask import Flask, render_template,jsonify,request
+from flask import Flask, render_template,request
+from flask_wtf import FlaskForm
+from wtforms import FileField,StringField
+from flask_uploads import configure_uploads, IMAGES, UploadSet
 from flask_sqlalchemy import SQLAlchemy
-from forms import EmailPasswordForm
-from werkzeug.utils import secure_filename
-import os
-
 app = Flask(__name__)
+
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = 'some$3cretKey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://icmwuvgxqphskl:29518807a50b98eff4637464f45393efd41a9e80b3dff925af7c55e28e967b28@ec2-54-210-128-153.compute-1.amazonaws.com:5432/d9u7e52sn8k5ti'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config["UPLOAD_FOLDER"] = "/uploads"
-
+app.config['UPLOADED_IMAGES_DEST']= "static/"
+images = UploadSet('images',IMAGES)
+configure_uploads(app,images)
 db = SQLAlchemy(app)
+
+class EmailPasswordForm(FlaskForm):
+    email = StringField('Email', )
+    fname = StringField('First Name')
+    lname = StringField('Last Name')
+    location = StringField('Location')
+    gender = StringField('Gender')
+    biography = StringField('Biography')
+    picture = FileField('image')    
+
+
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
@@ -21,6 +33,7 @@ class User(db.Model):
     email = db.Column(db.String(255))
     biography = db.Column(db.String(255))
     picture = db.Column(db.String(255))
+    #picture =db.Column(db.LargeBinary)
     #created_on = db.Column(db.DateTime, server_default=db.func.now())
     
     def __init__(self, id,fname, lname,gender,email,biography,picture):
@@ -49,19 +62,19 @@ def toJson(user):
 
 @app.route('/add', methods=['POST','GET']) 
 def profile_add():
-    form = EmailPasswordForm(request.form)
+    PhotoForm = EmailPasswordForm()
     if request.method == 'POST':
-        picture=form.picture.data
-        filename = secure_filename(picture.filename)
-        picture.save(os.path.join(app.config['UPLOAD_FOLDER'], picture))
-        data=User(1,form.fname.data,form.lname.data,form.gender.data,form.email.data,form.biography.data,"Test")
+        picture=PhotoForm.picture.data
+        print(picture)
+        filename = images.save(PhotoForm.picture.data)
+        filename= 'static/'+filename
+        data=User(1,PhotoForm.fname.data,PhotoForm.lname.data,PhotoForm.gender.data,PhotoForm.email.data,PhotoForm.biography.data,filename)
         db.session.add(data)
         db.session.flush()
         db.session.commit()
-        db.session.query(User).all()
-        return jsonify(toJson(data));
+        return profiles()
     if request.method == 'GET':
-        return render_template("addUser.html",form=form)
+        return render_template("addUser.html",form=PhotoForm)
     return "Form not validated"    
     
 
@@ -83,6 +96,7 @@ def profiles():
         dataJson.append(toJson(user))
     #return jsonify(dataJson)
     return render_template("viewProfiles.html",users=users)
+
 
 
 if __name__ == '__main__':
